773a0ce473cb419f74cf6e8edce64a1cbec32ae3
rafe721 <itsme.rahulap@gmail.com>
2017-04-05 11:02:25 +1200

    Revised the complete UI
    The entire UI has been revised to accomodate all showcasable projects.

---
+++ js/overlay.js
@@ -0,0 +1,30 @@
+
+/* Open when someone clicks on the span element */
+function openIconOverlay() {
+    document.getElementById("icon_overlay").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeIconOverlay() {
+    document.getElementById("icon_overlay").style.width = "0%";
+}
+
+/* Open when someone clicks on the span element */
+function openNeedOverlay() {
+    document.getElementById("need_overlay").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNeedOverlay() {
+    document.getElementById("need_overlay").style.width = "0%";
+}
+
+/* Open when someone clicks on the span element */
+function openImplOverlay() {
+    document.getElementById("impl_overlay").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeImplOverlay() {
+    document.getElementById("impl_overlay").style.width = "0%";
+}
